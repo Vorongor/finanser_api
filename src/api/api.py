6d7e779442d8fc7647f55cb0ae099b37fa1de2a9
Vorongor <mr.maddarknes@gmail.com>
2026-02-26 19:25:00 +0200

    Create Develop branch

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
 
-api_v1_router = APIRouter()
+from .user_router import user_router
+from ..config import get_settings
+
+settings = get_settings()
+
+api_v1_router = APIRouter(prefix=settings.API_V1_PREFIX)
+api_v1_router.include_router(user_router)
 
 
 @api_v1_router.get("/")
